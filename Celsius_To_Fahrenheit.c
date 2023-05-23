39c6e1762df0af453d87e690bb00a849c5a03a2e
pashpitha <121786468+pashpitha@users.noreply.github.com>
2023-05-23 11:56:03 +0530

    #include<stdio.h>  int main() {     float celsius,fahrenheit;
        scanf("%f",&celsius);
     
        
        fahrenheit=1.8*celsius+32;
       
        printf("%.2f",fahrenheit); 
        return 0;
    }

---
+++ Celsius_To_Fahrenheit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ int main()
+{
+    float celsius,fahrenheit;
+ 
+   
+
+    scanf("%f",&celsius);
+ 
+    
+    fahrenheit=1.8*celsius+32;
+   
+    printf("%.2f",fahrenheit); 
+    return 0;
+}
